5e5252522fff21a94eb9b3af29d2f0194f6a03ac
Brandon Leon <brandon.leon@servicenow.com>
2022-05-26 19:42:45 -0700

    Added sounds for each button.

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,18 +7,25 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func keyPressed(_ sender: UIButton) {
-        print("C button was pressed.")
+        playSound(soundName: sender.currentTitle!)
     }
     
-    
-
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
 
